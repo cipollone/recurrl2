90d205a5d254d3cd6ab7f1a566d87430db8e2869
Roberto Cipollone <cipollone.rt@gmail.com>
2021-07-29 18:44:03 +0200

    Added grid search; now running.
    + Added a tune field to run hyperparameter tuning.
      Use lists of lenght one for a single training.
    + Added other run options.
    + Running and converging on cheat mab.

--- recurrl/training.py
+++ recurrl/training.py
@@ -24,22 +24,25 @@ class Trainer:
         # Read parameters
         with open(params_file) as f:
             params = yaml.safe_load(f)
-        env_params = params["environment"]
-        alg_params = params["algorithm"]
+        self.env_params = params["environment"]
+        self.alg_params = params["algorithm"]
         self.logs_dir = params["logs-dir"]
         self.models_dir = params["model-dir"]
 
         # Trainer config
-        self.agent_type: str = alg_params["params"]["agent"]
-        self.agent_conf: dict = alg_params["params"]["config"]
+        self.agent_type: str = self.alg_params["params"]["agent"]
+        self.agent_conf: dict = self.alg_params["params"]["config"]
+
+        # Add choices to the configuration (see with_grid_search docstring)
+        with_grid_search(self.agent_conf, self.alg_params["params"]["tune"])
 
         # Env config
         self.agent_conf["env"] = NonMarkovEnvs
         self.agent_conf["env_config"] = dict(
-            name=env_params["name"],
+            name=self.env_params["name"],
             params=dict(
-                spec=env_params["params"]["spec"],
-                rdp=env_params["params"]["rdp"],
+                spec=self.env_params["params"]["spec"],
+                rdp=self.env_params["params"]["rdp"],
             ),
         )
 
@@ -61,4 +64,33 @@ class Trainer:
             self.agent_type,
             config=self.agent_conf,
             local_dir=self.logs_dir,
+            name="experiment",
+            **self.alg_params["params"]["run"],
         )
+
+
+def with_grid_search(conf, tune_conf):
+    """Compose the parameters to tune in the main configuration.
+
+    conf is any configuration of an agent (a dictionary). tune_conf is another
+    configuration in which values are lists instead of single elements. Any
+    list define a sequence of experiments, one for each value in the list.
+
+    NOTE: At least one list in tune_conf is always needed (even if of size 1)
+    otherwise tune library won't execute any experiment. I don't know why.
+    """
+    # Base case
+    if not isinstance(conf, dict) or not isinstance(tune_conf, dict):
+        return
+
+    # Scan conf
+    for key in conf:
+        if key in tune_conf:
+
+            # Iterate
+            with_grid_search(conf[key], tune_conf[key])
+
+            # Transform to search space
+            vals = tune_conf[key]
+            assert isinstance(vals, list), "'tune_conf' should contain lists"
+            conf[key] = tune.grid_search(vals)
